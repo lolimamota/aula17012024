726fe1121f5675f67d25211bbac9db61f8bd4a30
Lorrayne <lorraynelimamota@gmail.com>
2024-01-17 10:41:44 -0300

    Dinamica em grupo

---
+++ script.js
@@ -0,0 +1,18 @@
+//                  0        1              2          3
+let bandaArray = ["Ana", "Lorrayne", "Fabricia", "Leticia", "Juli"]
+//4
+
+bandaArray.push("Marta", "Grazi")
+
+bandaArray.splice(2,1, "Karynne")
+
+bandaArray.pop()
+
+bandaArray.push("Dani")
+
+bandaArray.shift()
+
+bandaArray.unshift("Carol")
+
+bandaArray.reverse()
+console.log(bandaArray)
