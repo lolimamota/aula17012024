2ac36852cb544d6c21f1e22cb3d37e917c5bc571
Lorrayne <lorraynelimamota@gmail.com>
2024-02-01 11:40:52 -0300

    modified: js files

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
-//                  0        1              2          3
-let bandaArray = ["Ana", "Lorrayne", "Fabricia", "Leticia", "Juli"]
-//4
+// //                  0        1              2          3
+// let bandaArray = ["Ana", "Lorrayne", "Fabricia", "Leticia", "Juli"]
+// //4
 
-bandaArray.push("Marta", "Grazi")
+// bandaArray.push("Marta", "Grazi")
+// console.log(bandaArray)
 
-bandaArray.splice(2,1, "Karynne")
+// bandaArray.splice(2,1, "Karynne")
 
-bandaArray.pop()
+// bandaArray.pop()
 
-bandaArray.push("Dani")
+// bandaArray.push("Dani")
 
-bandaArray.shift()
+// bandaArray.shift()
 
-bandaArray.unshift("Carol")
+// bandaArray.unshift("Carol")
 
-bandaArray.reverse()
-console.log(bandaArray)
+// bandaArray.reverse()
+
+// let fruta = "maçã";
+
+// switch(fruta) {
+//     case "banana": 
+//         console.log("vc escolheu bananinha");
+//         break;
+//     case "maçã": 
+//         console.log("vc escolheu maçã");
+        
+// }
+
+// let hora = 9;
+
+//   hora++;
+
+//   if(hora <= 10){
+
+//    console.log(hora++);
+
+//  } else {
+
+//  console.log("Tenha uma ótima noite");
+
+// }  
+
+// let nome = "Lucas";
+
+// if(nome == "Lucas") {
+
+//   echo "Esse é o nome do Professor";
+
+// }
+
+// function sub(a, b) {
+//     resultado = 2*(a - b);
+//      return resultado;
+//   }
+  
+//    console.log(sub(1,1));
+  
+//    let hora = 0;
+
+//    if (hora < 12) {
+//      console.log("Bom dia!");
+//    } else if (hora = 18) {
+//      console.log("Boa tarde!");
+//    } else {
+//      console.log("Boa noite!");
+//  }
+
+let hora = 9;
+
+  hora++;
+
+  if(hora <= 10){
+
+   console.log(hora++);
+
+ } else {
+
+ console.log("Tenha uma ótima noite");
+
+}  
